7fcc39f6a64e7ce8c66e47d92cf114f199f3985b
Gorstak-79 <122930303+Gorstak-79@users.noreply.github.com>
2023-03-07 06:02:04 +0100

    Delete Elevate.bat

--- Elevate.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-
->nul 2>&1 fsutil dirty query %systemdrive% || echo CreateObject^("Shell.Application"^).ShellExecute "%~0", "ELEVATED", "", "runas", 1 > "%temp%\uac.vbs" && "%temp%\uac.vbs" && exit /b
-DEL /F /Q "%temp%\uac.vbs"
